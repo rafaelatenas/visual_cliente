c6cb5dc316595b6dc2d52a783c38612dd8775372
rafaelatenas <rafaelorozco@atenasconsultores.com>
2022-04-07 17:03:44 -0400

    master

--- src/componentes/Admin/componentesAdmin/Usuario.js
+++ src/componentes/Admin/componentesAdmin/Usuario.js
@@ -10,86 +10,84 @@ import CrearUsuario from "./crearUsuario";
 
 class Usuario extends React.Component{
 
-    
-
 
     componentDidMount(){
-        // Inicio de animación del formulario
-        var current_fs, next_fs, previous_fs;
-        var left, opacity, scale;
-        var animating; 
+        // // Inicio de animación del formulario
+        // var current_fs, next_fs, previous_fs;
+        // var left, opacity, scale;
+        // var animating;
 
-        $(".next").on('click',(function(){
-            if(animating) return false;
-            animating = true;
-            
-            current_fs = $(this).parent();
-            next_fs = $(this).parent().next();
-            
-            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-            
-            next_fs.show(); 
-            current_fs.animate({opacity: 0}, {
-                step: function(now, mx) {
-                    
-                    scale = 1 - (1 - now) * 0.2;
-                    
-                    left = (now * 50)+"%";
-                    opacity = 1 - now;
-                    current_fs.css({
-                'transform': 'scale('+scale+')',
-                'position': 'absolute',
-                'top': '15%'
-            });
-                    next_fs.css({'left': left, 'opacity': opacity});
-                }, 
-                duration: 800, 
-                complete: function(){
-                    current_fs.hide();
-                    animating = false;
-                }, 
-                easing: 'easeInOutBack'
-            });
-        }));
+        // $(".next").on('click',(function(){
+        //     if(animating) return false;
+        //     animating = true;
 
-        $(".previous").on('click',(function(){
-            if(animating) return false;
-            animating = true;
-            
-            current_fs = $(this).parent();
-            previous_fs = $(this).parent().prev();
-            
-            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-            
-            previous_fs.show(); 
-            current_fs.animate({opacity: 0}, {
-                step: function(now, mx) {
-                    scale = 0.8 + (1 - now) * 0.2;
-                    left = ((1-now) * 50)+"%";
-                    opacity = 1 - now;
-                    current_fs.css({'left': left});
-                    previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-                }, 
-                duration: 800, 
-                complete: function(){
-                    current_fs.hide();
-                    animating = false;
-                }, 
-                //this comes from the custom easing plugin
-                easing: 'easeInOutBack'
-            });
-        }));
+        //     current_fs = $(this).parent();
+        //     next_fs = $(this).parent().next();
 
-        $(".submit").on('click',(function(){
-            return false;
-        }))
+        //     $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-        // Fin de la animación del formulario
+        //     next_fs.show();
+        //     current_fs.animate({opacity: 0}, {
+        //         step: function(now, mx) {
+
+        //             scale = 1 - (1 - now) * 0.2;
+
+        //             left = (now * 50)+"%";
+        //             opacity = 1 - now;
+        //             current_fs.css({
+        //         'transform': 'scale('+scale+')',
+        //         'position': 'absolute',
+        //         'top': '15%'
+        //     });
+        //             next_fs.css({'left': left, 'opacity': opacity});
+        //         },
+        //         duration: 800,
+        //         complete: function(){
+        //             current_fs.hide();
+        //             animating = false;
+        //         },
+        //         easing: 'easeInOutBack'
+        //     });
+        // }));
+
+        // $(".previous").on('click',(function(){
+        //     if(animating) return false;
+        //     animating = true;
+
+        //     current_fs = $(this).parent();
+        //     previous_fs = $(this).parent().prev();
+
+        //     $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+        //     previous_fs.show();
+        //     current_fs.animate({opacity: 0}, {
+        //         step: function(now, mx) {
+        //             scale = 0.8 + (1 - now) * 0.2;
+        //             left = ((1-now) * 50)+"%";
+        //             opacity = 1 - now;
+        //             current_fs.css({'left': left});
+        //             previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+        //         },
+        //         duration: 800,
+        //         complete: function(){
+        //             current_fs.hide();
+        //             animating = false;
+        //         },
+        //         //this comes from the custom easing plugin
+        //         easing: 'easeInOutBack'
+        //     });
+        // }));
+
+        // $(".submit").on('click',(function(){
+        //     return false;
+        // }))
+
+        // // Fin de la animación del formulario
 
         // Inicio de animación y efecto de despliegue de las opciones de administrador
         const botones = document.querySelectorAll('.boton');
         const contenedores = document.querySelectorAll('.contenedor')
-        
+
             for (let j = 0; j < botones.length; j++) {
                 botones[j].addEventListener('click',()=>{
 
@@ -106,7 +104,7 @@ class Usuario extends React.Component{
                         contenedores[j].style.borderTopLeftRadius= '0em'
                         contenedores[j].style.borderBottomRightRadius= '0.3em'
                         contenedores[j].style.borderBottomLeftRadius= '0.3em'
-                   
+
                       contenedores[j].animate([
                           {height:'75%'},
                       ],{
@@ -120,7 +118,7 @@ class Usuario extends React.Component{
                                 botones[j].style.borderBottomRightRadius= '0.3em';
                                 botones[j].style.borderBottomLeftRadius= '0.3em';
                             }, 1400);
-                      
+
                       contenedores[j].animate([
                           {height:'0%'},
                       ],{
@@ -129,7 +127,7 @@ class Usuario extends React.Component{
                       });
 
                       break;
-                    
+
                     default:
                         break;
                 }
@@ -145,7 +143,7 @@ class Usuario extends React.Component{
                         botonCreacion.style.borderBottomRightRadius= '0.3em';
                         botonCreacion.style.borderBottomLeftRadius= '0.3em';
                     }, 1400);
-              
+
                     contenedorCreacion.animate([
                         {height:'0%'},
                     ],{
@@ -160,7 +158,7 @@ class Usuario extends React.Component{
                         botonConsulta.style.borderBottomRightRadius= '0.3em';
                         botonConsulta.style.borderBottomLeftRadius= '0.3em';
                     }, 1400);
-              
+
                     contenedorConsulta.animate([
                         {height:'0%'},
                     ],{
@@ -170,7 +168,7 @@ class Usuario extends React.Component{
                 }
             })
             }
-        // Fin de animación y efecto de despliegue de las opciones de administrador    
+        // Fin de animación y efecto de despliegue de las opciones de administrador
     }
 
     render(){
@@ -194,7 +192,7 @@ class Usuario extends React.Component{
             </section>
             </div>
             </>
-            
+
         )
     }
 
--- src/componentes/Admin/componentesAdmin/crearUsuario.js
+++ src/componentes/Admin/componentesAdmin/crearUsuario.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { FormErrors } from "../FormErrors"
+import { $ } from "react-jquery-plugin";
 import Swal from "sweetalert2";
 import withReactContent from 'sweetalert2-react-content';
+
 
 
 export default class crearUsuario extends React.Component{
@@ -41,40 +42,55 @@ export default class crearUsuario extends React.Component{
         let emailValid = this.state.emailValid;
         let passwordValid = this.state.passwordValid;
        // let confirmacionpasswordValid = this.state.confirmacionpasswordValid;
-        switch(fieldName) {
 
-          case 'Nombres':
-            nombresValid = value.length >= 6;
-            fieldValidationErrors.Nombres = nombresValid ? '' : ' es demasiado corto';
-            console.log(this.state.formErrors.Nombres)
+       const MySwal = withReactContent(Swal)
+       const toast = MySwal.mixin({
+         toast: true,
+         position: 'top-end',
+         showConfirmButton: false,
+         timer: 10000,
+         timerProgressBar: true,
+         didOpen: (toast) => {
+             toast.addEventListener('mouseenter', Swal.stopTimer)
+             toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+       });  
 
-            break;
-            case 'Apellidos':
-                apellidosValid = value.length >= 6;
-                fieldValidationErrors.Apellidos = apellidosValid ? '' : ' es demasiado corto';
-                console.log(this.state.formErrors.Apellidos)
-                
-            break;  
-          case 'Email':
-            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-            fieldValidationErrors.Email = emailValid ? '' : ' es invalido';
-            console.log(this.state.formErrors.Email)
-            
-            break;
-          case 'Password':
-            passwordValid = value.length >= 6;
-            fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
-            console.log(this.state.formErrors.Password)
-            break;
-        //   case 'ConfirmacionPassword':
-        //     confirmacionpasswordValid = value.length >= 6;
-        //     fieldValidationErrors.ConfirmacionPassword = confirmacionpasswordValid ? '': ' es demasiado corto';
-        //     break;  
-          default:
-            break;
-                
-    
+       switch(fieldName) {
+
+            case 'Nombres':
+              nombresValid = value.length >= 6;
+              fieldValidationErrors.Nombres = nombresValid ? '' : ' es demasiado corto';
+              
+              break;
+              case 'Apellidos':
+                  apellidosValid = value.length >= 6;
+                  fieldValidationErrors.Apellidos = apellidosValid ? '' : ' es demasiado corto';
+              break;  
+            case 'Email':
+              emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+              fieldValidationErrors.Email = emailValid ? '' : ' es invalido';
+              toast.fire({
+                icon: 'error',
+                title: ''+fieldValidationErrors.Email+'',
+                confirmButtonText: `Ok`,
+              })
+              break;
+            case 'Password':
+              passwordValid = value.length >= 6;
+              fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
+              break;
+          //   case 'ConfirmacionPassword':
+          //     confirmacionpasswordValid = value.length >= 6;
+          //     fieldValidationErrors.ConfirmacionPassword = confirmacionpasswordValid ? '': ' es demasiado corto';
+          //     break;  
+            default:
+              break;
+
+                  
+      
         }
+
         this.setState(
           {
             formErrors: fieldValidationErrors,
@@ -90,20 +106,101 @@ export default class crearUsuario extends React.Component{
         return(error.length === 0 ? '' : 'has-error');
       }
       validateForm() {
+        //--- Para botón Next ---//
+        this.setState({formValidNext: this.state.nombresValid && this.state.apellidosValid});
+
+        //--- Para botón Submit ---//
         this.setState({formValid: this.state.nombresValid && this.state.apellidosValid && this.state.emailValid && this.state.passwordValid });
+
       }
       handleUserInput = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         this.setState({[name]: value},() => { this.validateField(name, value) });
       }  
-    //   componentDidMount(){
-    //       const prub = document.querySelectorAll('input');
-    //       console.dir(prub[0].onchange)
-    //   }
-    enviarDatos=()=>{
-        console.log(22)
-    }
+       
+    enviarDatos=(e)=>{
+      console.log("Fomulario Enviado....");
+  }
+ 
+
+componentDidMount(){
+
+   // Inicio de animación del formulario
+   var current_fs, next_fs, previous_fs;
+   var left, opacity, scale;
+   var animating;
+
+   $(".next").on('click',(function(){
+       if(animating) return false;
+       animating = true;
+
+       current_fs = $(this).parent();
+       next_fs = $(this).parent().next();
+
+       $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+       next_fs.show();
+       current_fs.animate({opacity: 0}, {
+           step: function(now, mx) {
+
+               scale = 1 - (1 - now) * 0.2;
+
+               left = (now * 50)+"%";
+               opacity = 1 - now;
+               current_fs.css({
+           'transform': 'scale('+scale+')',
+           'position': 'absolute',
+           'top': '15%'
+       });
+               next_fs.css({'left': left, 'opacity': opacity});
+           },
+           duration: 800,
+           complete: function(){
+               current_fs.hide();
+               animating = false;
+           },
+           easing: 'easeInOutBack'
+       });
+   }));
+
+   $(".previous").on('click',(function(){
+       if(animating) return false;
+       animating = true;
+
+       current_fs = $(this).parent();
+       previous_fs = $(this).parent().prev();
+
+       $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+       previous_fs.show();
+       current_fs.animate({opacity: 0}, {
+           step: function(now, mx) {
+               scale = 0.8 + (1 - now) * 0.2;
+               left = ((1-now) * 50)+"%";
+               opacity = 1 - now;
+               current_fs.css({'left': left});
+               previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+           },
+           duration: 800,
+           complete: function(){
+               current_fs.hide();
+               animating = false;
+           },
+           //this comes from the custom easing plugin
+           easing: 'easeInOutBack'
+       });
+       $(".submit").on('click',(function(){
+             alert(2322)
+         }))
+   }));
+
+   
+
+   // Fin de la animación del formulario
+
+}
+
     render(){
         return(
             <form id="msform">
@@ -117,16 +214,17 @@ export default class crearUsuario extends React.Component{
                 
                 <input type="text" name="Nombres" placeholder="Nombres" value={this.state.Nombres} onChange={this.handleUserInput}/>
                 <input type="text" name="Apellidos" placeholder="Apellidos" value={this.state.Apellidos} onChange={this.handleUserInput}/>
-                <input type="button" name="next" className="next action-button" value="Next" />
+                <input type="button" name="next" disabled={!this.state.formValidNext} className="next action-button" value="Next" />
             </fieldset>
             <fieldset>
                 <h2 className="fs-title">Crear Usuario</h2>
                 <input  type="email" name="Email" placeholder="Correo" value={this.state.Email} onChange={this.handleUserInput}/>
                 <input className={`${this.errorClass(this.state.formErrors.Password)}`} type="password" name="Password" placeholder="Contraseña" value={this.state.Password} onChange={this.handleUserInput}/>
                 <input type="button" name="previous" className="previous action-button" value="Previous" />
-                <input type="submit" name="submit" className="submit action-button" value="Submit" onClick={this.enviarDatos}/>
+                <input type="submit" disabled={!this.state.formValid} name="submit" className="submit action-button" onClick={this.enviarDatos}  value="Submit" />
             </fieldset>
         </form> 
         )
     }
 }
+
