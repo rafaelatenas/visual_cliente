0c9a5234a8f8d4a02dc7608b5cbdd93f9cf7f9f8
rafaelatenas <rafaelorozco@atenasconsultores.com>
2022-04-30 13:38:52 -0400

    Arreglos datatable

--- src/componentes/Admin/componentesAdmin/usuario/ListarUsuarios.js
+++ src/componentes/Admin/componentesAdmin/usuario/ListarUsuarios.js
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
 //import './App.css';
 import axios from 'axios';
-
-import { makeStyles } from '@mui/styles';
-
-import {Table, TableContainer, TableHead, TableCell, TableBody, TableRow, Modal, Button, TextField} from '@mui/material';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import './listarUsuarios.css'
+import { makeStyles } from '@material-ui/core/styles';
+import {Table, TableContainer, TableHead, TableCell, TableBody, TableRow, Modal, Button, TextField} from '@material-ui/core';
+import { Edit , Delete, Check, Close} from '@material-ui/icons';
 import ReactDOM from 'react-dom';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
   inputMaterial:{
     width: '95%'
   }
+  
 }));
 
 function ListarUsuarios() {
@@ -37,10 +37,13 @@ const styles= useStyles();
   const [modalEliminar, setModalEliminar]=useState(false);
 
   const [consolaSeleccionada, setConsolaSeleccionada]=useState({
+    
     nombre: '',
-    empresa:'',
-    lanzamiento: '',
-    unidades_vendidas: ''
+    apellidos:'',
+    correo: '',
+    usuario: '',
+    id_usuario:'',
+
   })
 
   const handleChange=e=>{
@@ -138,6 +141,7 @@ const styles= useStyles();
   useEffect(async()=>{
     await peticionGet();
   },[])
+  
 
   const bodyInsertar=(
     <div className={styles.modal}>
@@ -181,19 +185,21 @@ const styles= useStyles();
       </fieldset>
     </div>
   );
- 
+  
 
   const bodyEditar=(
     <div className={styles.modal}>
       <h3>Editar Consola</h3>
       <div className='agruparEdit'>
-        <TextField name="nombre" className={styles.inputMaterial} label="Nombre" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.id_usuario}/>
+        <TextField name="id_usuario" className={styles.inputMaterial} label="ID Usuario" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.id_usuario}/>
         <br />
-        <TextField name="empresa" className={styles.inputMaterial} label="Empresa" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.usuario}/>
+        <TextField name="nombre" className={styles.inputMaterial} label="Nombre" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.nombre}/>
         <br />
-        <TextField name="lanzamiento" className={styles.inputMaterial} label="Lanzamiento" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.nombres}/>
+        <TextField name="apellidos" className={styles.inputMaterial} label="Apellidos" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.apellidos}/>
         <br />
-        <TextField name="unidades_vendidas" className={styles.inputMaterial} label="Unidades Vendidas" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.apellidos}/>
+        <TextField name="correo" className={styles.inputMaterial} label="Correo" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.correo}/>
+        <br />
+        <TextField name="usuario" className={styles.inputMaterial} label="Usuario" onChange={handleChange} value={consolaSeleccionada && consolaSeleccionada.correo}/>
         <br /><br />
       </div>
       <div align="right">
@@ -221,33 +227,43 @@ const styles= useStyles();
       <br />
     <Button onClick={()=>abrirCerrarModalInsertar()}>Insertar</Button>
       <br /><br />
-     <TableContainer style={{display: 'table', tableLayout:'fixed', width:'100%'}}>
-       <Table>
+     <TableContainer  style={{display: 'flex',flexDirection:'column' , tableLayout:'fixed', width:'100%', height:'370px', fontFamily:'Lucida Sans Demibold Roman'}}>
+       <Table style={{display: 'flex',flexDirection:'column'}}>
          <TableHead>
            <TableRow>
-             <TableCell>Nombre</TableCell>
-             <TableCell>Empresa</TableCell>
-             <TableCell>Año de Lanzamiento</TableCell>
-             <TableCell>Unidades Vendidas (millones)</TableCell>
-             <TableCell>Acciones</TableCell>
+             <TableCell style={{textAlign:'center'}}>ID Usuario</TableCell>
+             <TableCell>Usuario</TableCell>
+             <TableCell>Nombres</TableCell>
+             <TableCell>Apellidos</TableCell>
+             <TableCell>Correo</TableCell>
+             <TableCell>Fecha de Creacion</TableCell>
+             <TableCell>Usuario Confirmado</TableCell>
+             <TableCell>Usuario Activo</TableCell>
+             <TableCell>Fechad de Modificacion</TableCell>
+
            </TableRow>
          </TableHead>
 
-         <TableBody>
+         <TableBody className='tableContainer' style={{overflowY:'scroll'}}>
            {data.map(consola=>(
-             <TableRow key={consola.id_usuario}>
+             <TableRow style={{overflowY:'scroll', width:'100%'}} key={consola.id_usuario}>
                <TableCell>{consola.id_usuario}</TableCell>
                <TableCell>{consola.usuario}</TableCell>
+               <TableCell>{consola.nombres}</TableCell>
+               <TableCell>{consola.apellidos}</TableCell>
+               <TableCell>{consola.correo}</TableCell>
+               <TableCell>{new Date(consola.fecha_creacion).toLocaleDateString()}</TableCell>
+               <TableCell>{consola.Ind_Activo ? <Check></Check> : <Close></Close> }</TableCell>
+               <TableCell>{consola.Ind_Us_Activo ? <Check></Check> : <Close></Close> }</TableCell>
+               <TableCell>{new Date(consola.fecha_creacion).toLocaleDateString()}</TableCell>
+               
                {/* <TableCell>{consola.nombres}</TableCell>
                <TableCell>{consola.apellidos}</TableCell>
                <TableCell>{consola.correo}</TableCell> */}
                <TableCell>
-                    <ModeEditIcon className={styles.iconos} onClick={()=>seleccionarConsola(consola, 'Editar')} />
-        
-                 {/* <Edit className={styles.iconos} onClick={()=>seleccionarConsola(consola, 'Editar')}/> */}
-                 &nbsp;&nbsp;&nbsp;
-                 {/* <Delete  className={styles.iconos} onClick={()=>seleccionarConsola(consola, 'Eliminar')}/> */}
-                 </TableCell> 
+                <Edit className={styles.iconos} onClick={()=>seleccionarConsola(consola, 'Editar')}/>
+                <Delete  className={styles.iconos} onClick={()=>seleccionarConsola(consola, 'Eliminar')}/>
+               </TableCell> 
              </TableRow>
            ))}
          </TableBody>
@@ -276,363 +292,4 @@ const styles= useStyles();
 }
 
 export default ListarUsuarios;
-
-// class ListarUsuarios extends React.Component{
-
-//   constructor(props) {
-//     super(props)            
-//       this.state = {
-//         Usuarios:[],
-//         idUsuario:'',
-//         Nombres:[]
-//       }
-//     }
-
-
-    
-
-
-//   componentDidMount() {
-//         //Listar Usuario//
-//         const MySwal = withReactContent(Swal)
-//         const toast = MySwal.mixin({
-//         toast: true,
-//         position: 'top=end',
-//         showConfirmButton: false,
-//         timer: 10000,
-//         timerProgressBar: true,
-//           didOpen: (toast) => {
-//             toast.addEventListener('mouseenter', Swal.stopTimer)
-//             toast.addEventListener('mouseleave', Swal.resumeTimer)
-//           }
-//         });
-        
-//         var token=localStorage.getItem('token');
-//         axios.get(process.env.REACT_APP_API_ENDPOINT+'ListarUsuarios',{
-//           headers: {
-//             'Authorization': `Bearer ${token}`
-//           },
-//         })
-//           .then(res => {
-//             this.setState({ Usuarios: res.data.data });
-//             resultado(res.data.data)
-//           })
-
-//           .catch((error) => {
-//                 console.error(error)
-//                 console.log(error.response.data.message);
-//                 console.log(error.response.status);
-//                 console.log(error.response.headers);
-//                 toast.fire({
-//                 icon: 'error',
-//                 title: ''+error.response.data.message+'',
-//                 confirmButtonText: `Ok`,
-//                 })
-//           })
-//         document.getElementById('pantalla').addEventListener('click',()=>{
-//           document.getElementById('BoxActualizar').style.display='none'
-//           document.getElementById('pantalla').style.opacity='1'
-//         });
-//         const resultado = data => {
-//           const elementoHTML = document.getElementById('boton');
-
-//           for (const iterator of data) {
-//             console.log(iterator)
-//             ActivoUS =  iterator.Ind_Us_Activo
-//             let Activo = '';
-//             switch (ActivoUS) {
-//               case true:
-//                 Activo +=`           
-//                 <img src=${check} class="boton" alt="Usuario Activo"></img>
-//               ` 
-//               elementoHTML.innerHTML = Activo;
-//                 break;
-//                 case false:
-//                   Activo +=`           
-//                   <img src=${check} class="boton" alt="Usuario Activo"></img>
-//                 ` 
-//                 elementoHTML.innerHTML = Activo;
-//                   break;
-//               default:
-//                 break;
-//             }
-            
-//           }
-//           const Ind_US_Activo =  document.querySelectorAll('.Ind-US-Activo ')
-//           for (let w = 0; w < (Ind_US_Activo.length -1); w++) {
-//             const elementoHTML = Ind_US_Activo[w+1];
-//             var ActivoUS = data[w].Ind_Us_Activo;
-//             let Activo = '';
-//             if (ActivoUS === true) {
-//               Activo +=`           
-//                 <img src=${check} class="boton" alt="Usuario Activo"></img>
-//               ` 
-//               elementoHTML.innerHTML = Activo;
-
-//             } else {
-//               Activo +=`           
-//                 <img src=${eliminar} class="boton" alt="Usuario Activo"></img>
-//               ` 
-//               elementoHTML.innerHTML = Activo;
-//             }
-//           }
-          
-//           const Ind_Activo =  document.querySelectorAll('.Ind_Activo')
-//           for (let w = 0; w < (Ind_Activo.length -1); w++) {
-//             const elementoHTML = Ind_Activo[w+1];
-//             var ActivoIND = data[w].Ind_Activo;
-//             let Activo = '';
-//             if (ActivoIND === true) {
-//               Activo +=`           
-//                 <img src=${check} class="boton" alt="Usuario Activo"></img>
-//               ` 
-//               elementoHTML.innerHTML = Activo;
-
-//             } else {
-//               Activo +=`           
-//                 <img src=${eliminar} class="boton" alt="Usuario Activo"></img>
-//               ` 
-//               elementoHTML.innerHTML = Activo;
-//             }
-//           }
-//         }
-//         $(document).on(function () {
-//           setTimeout(function(){
-//           $('#TableUsuarios').DataTable(
-//               {
-//                   "order": [[ 0, "asc" ]],
-//                   "lengthMenu": [
-//                       [10, 25, 50, 100, -1],
-//                       [10, 25, 50, 100, "All"]
-//                   ],
-//                   "language": {
-//                       "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-//                   },
-                  
-//                   "bDestroy": true,                        
-//                   "autoWidth": true,
-//                   "responsive": true,
-//                   "dom": '<"top"lBf>rt<"bottom"ip>',
-//                   "buttons": ['excelHtml5','csvHtml5','print'],
-//               }
-//           );
-//            } ,1000);
-//       });
-
-
-
-     
-//   }
-
-  
-//   validateField(fieldName, value) {
-//       let fieldValidationErrors = this.state.formErrors;
-//       let nombresValid= this.state.nombresValid;
-//       let apellidosValid= this.state.apellidosValid;
-//       let correoValid = this.state.correoValid;
-//       let passwordValid = this.state.passwordValid;
-//       //let id_usuarioValid = this.state.id_usuarioValid;
-
-//      const MySwal = withReactContent(Swal)
-//      const toast = MySwal.mixin({
-//        toast: true,
-//        position: 'top-end',
-//        showConfirmButton: false,
-//        timer: 10000,
-//        timerProgressBar: true,
-//        didOpen: (toast) => {
-//            toast.addEventListener('mouseenter', Swal.stopTimer)
-//            toast.addEventListener('mouseleave', Swal.resumeTimer)
-//         }
-//      });  
-
-//      switch(fieldName) {
-
-//           case 'nombres':
-//             nombresValid = value.length >= 6;
-//             fieldValidationErrors.Nombres = nombresValid ? '' : ' es demasiado corto';
-            
-//             break;
-//             case 'apellidos':
-//                 apellidosValid = value.length >= 6;
-//                 fieldValidationErrors.Apellidos = apellidosValid ? '' : ' es demasiado corto';
-//             break;  
-//           case 'correo':
-//             correoValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-//             fieldValidationErrors.correo = correoValid ? '' : ' es invalido';
-//             toast.fire({
-//               icon: 'error',
-//               title: ''+fieldValidationErrors.correo+'',
-//               confirmButtonText: `Ok`,
-//             })
-//             break;
-//           case 'password':
-//             passwordValid = value.length >= 6;
-//             fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
-//             break;
-//         //   case 'Confirmacionpassword':
-//         //     confirmacionpasswordValid = value.length >= 6;
-//         //     fieldValidationErrors.Confirmacionpassword = confirmacionpasswordValid ? '': ' es demasiado corto';
-//         //     break;  
-//           default:
-//             break;
-
-//       }
-
-//       this.setState(
-//         {
-//           formErrors: fieldValidationErrors,
-//           nombresValid:nombresValid,
-//           apellidosValid:apellidosValid,
-//           correoValid: correoValid,
-//           passwordValid: passwordValid,
-//         }, 
-//         this.validateForm
-//         );
-//   }
-
-//   errorClass(error) {
-//       return(error.length === 0 ? '' : 'has-error');
-//   }
-
-//   validateForm() {
-//       //--- Para botón Next ---//
-//       this.setState({formValidNext: this.state.nombresValid && this.state.apellidosValid});
-
-//       //--- Para botón Submit ---//
-//       this.setState({formValid: this.state.nombresValid && this.state.apellidosValid && this.state.correoValid && this.state.passwordValid });
-
-//   }
-     
-//   handleUserInput = (e) => {
-//       const name = e.target.name;
-//       const value = e.target.value;
-//       this.setState({[name]: value});
-//       console.log(value)
-//   }  
-//   editEmployee(id){
-
-//       document.getElementById('BoxActualizar').style.display = 'block';
-//       document.getElementById('pantalla').style.opacity = '.3';
-
-//       console.log(id);
-//       var token=localStorage.getItem('token');
-//       axios.get(process.env.REACT_APP_API_ENDPOINT+"ListarUsuariosId/"+id+"",{
-//         headers: {
-//           'Authorization': `Bearer ${token}`
-//           },
-//         }).then(result => {
-          
-
-//           this.setState({ Nombres: result.data.data[0].nombres});
-//        console.log(this.state.Nombres)
-
-//        const elementoHTML = (
-//         <input name="Nombres" id="Nombres" value={this.state.Nombres} onChange={this.handleUserInput} placeholder="Nombres"></input>
-//         )
-         
-//         ReactDOM.render(
-//           elementoHTML,
-//           document.getElementById('BoxActualizar')
-//         );
-
-//       }).catch(err => {
-//         if (err.response) {
-//           console.log(err.response.data.message);
-//           console.log(err.response.status);
-//           console.log(err.response.headers);        
-//         }
-//       })
-//       console.log(this.state.Nombres)
-      
-//   };
-  
-//   columns = [
-    
-//     {
-//       name: 'ID',
-//       selector: row => row.id_usuario,
-//       sortable: true,
-      
-//     },
-//     { 
-//       name: 'Usuario',
-//       selector: row => row.usuario,
-//       sortable: true,
-//       grow: 2,
-//     },
-//     {
-//       name: 'Nombres',
-//       selector: row => row.nombres,
-//       sortable: true,
-//       grow: 2,
-//     },
-//     { 
-//       name: 'Apellidos',
-//       selector: row => row.apellidos,
-//       sortable: true,
-//       grow: 2,
-//     },
-//     {
-//       name: 'Correo',
-//       selector: row => row.correo,
-//       sortable: true,
-//       grow: 2,
-//     },
-//     {
-//       name: 'Creacion',
-//       selector: row => new Date(row.fecha_creacion).toLocaleDateString(),
-//       sortable: true,
-//       //grow: 2,
-      
-//     },
-//     {
-      
-//       cell: () => 
-           
-//       <button id="boton"></button>,
-//       ignoreRowClick: true,
-//       allowOverflow: true,
-//       button: true,
-//     },
-//     {
-      
-
-//       cell: () => 
-//       <div id="imgContenedor"></div>,      
-//       ignoreRowClick: true,
-//       allowOverflow: true,
-//       button: true,
-//     },
-//   ]
-  
-//    MyComponent = () => (
-//     <DataTable
-//       columns={this.columns}
-//     />
-//   );   
-       
-//     render() {
-      
-//       return (
-//           <>
-//             <h1>Actualizar Datos de Usuario</h1>
-//               <DataTable 
-//                 columns={this.columns}
-//                 data={this.state.Usuarios}
-//                 fixedHeader
-//                 fixedHeaderScrollHeight="300px"
-//                 highlightOnHover
-//                 pagination
-//                 responsive
-//                 subHeader
-//                 subHeaderAlign="right"
-//                 subHeaderWrap
-//             />
-//         </>
-//       )
-//     }
-// }
-// export default ListarUsuarios;
-
 
