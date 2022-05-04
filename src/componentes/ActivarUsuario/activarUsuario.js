24e43bf4c05cd69fdfc59a18ed185cc8bd7a2261
rafaelatenas <rafaelorozco@atenasconsultores.com>
2022-05-04 11:28:57 -0400

    Ajustes 04/05/2022

--- src/componentes/ActivarUsuario/activarUsuario.js
+++ src/componentes/ActivarUsuario/activarUsuario.js
@@ -1,188 +1,167 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import eye_slash from '../../landing/favicon/eye-slash-solid.svg';
-import eye_solid from '../../landing/favicon/eye-solid.svg';
 import axios from 'axios';
 import withReactContent from 'sweetalert2-react-content';
 import Swal from 'sweetalert2';
 import './activarUsuario.css';
-
+import { TextField, FormControl, InputLabel, OutlinedInput} from '@mui/material';
+import { Visibility, VisibilityOff} from '@material-ui/icons';
+import { IconButton, Button} from '@mui/material';
 
 class ActivarUsuario extends React.Component{
-
-    constructor(props) {
-        super(props)            
-          this.state = {
-            formErrors: {
-                Usuario:'',
-                Password: '',
-                ConfirmacionPassword:'',
-                },
-            usuarioValid:false,
-            passwordValid: false,
-            confirmacionpasswordValid: false,
-            Usuario:'',
-            Password: '',
-            ConfirmacionPassword:'',
-            } 
-        }
+   
+  constructor(props) {
+    super(props)            
+    this.state = {
+      formErrors: {
+        Usuario:'',
+        password: '',
+        confirmacionpassword:'',
+      },
+        usuarioValid:false,
+        passwordValid: false,
+        confirmacionpasswordValid: false,
+        formValid: false,
+        showPassword: false,
+        IgualPassword:false,        
+        Usuario:'',
+        password: '',
+        confirmacionpassword:'',
+    } 
+  }
         
 
-        validateField(fieldName, value) {
-            let fieldValidationErrors = this.state.formErrors;
-            let usuarioValid = this.state.usuarioValid;
-            let passwordValid = this.state.passwordValid;
-            let confirmacionpasswordValid = this.state.confirmacionpasswordValid;
-      
-           const MySwal = withReactContent(Swal)
-           const toast = MySwal.mixin({
-             toast: true,
-             position: 'top-end',
-             showConfirmButton: false,
-             timer: 10000,
-             timerProgressBar: true,
-             didOpen: (toast) => {
-                 toast.addEventListener('mouseenter', Swal.stopTimer)
-                 toast.addEventListener('mouseleave', Swal.resumeTimer)
-              }
-           });  
-      
-           switch(fieldName) {
-            case 'Email':
-              usuarioValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-              fieldValidationErrors.Usuario = usuarioValid ? '' : ' es invalido';
-              break;
-            case 'Password':
-                passwordValid = value.length >= 6;
-                fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
-                break;
-            case 'ConfirmacionPassword':
-                confirmacionpasswordValid = value.length >= 6;
-                fieldValidationErrors.ConfirmacionPassword = confirmacionpasswordValid ? '': ' es demasiado corto';
-                break;
-            default:
-                break;
-      
-            }
-      
-            this.setState(
-              {
-                formErrors: fieldValidationErrors,
-                usuarioValid: usuarioValid,
-                passwordValid: passwordValid,
-                confirmacionpasswordValid: confirmacionpasswordValid
-              }, 
-              this.validateForm
-              );
-          }
-          errorClass(error) {
-            return(error.length === 0 ? '' : 'has-error');
-          }
-          validateForm() {
-            this.setState({formValid: this.state.passwordValid});
-          }
-          
-          handleUserInput = (e) => {
-            const name = e.target.name;
-            const value = e.target.value;
-            this.setState({[name]: value},() => { this.validateField(name, value) });
-          }   
-          
-          enviarDatos=(e)=>{ 
-            const MySwal = withReactContent(Swal)
-            const toast = MySwal.mixin({
-              toast: true,
-              position: 'top-end',
-              showConfirmButton: false,
-              timer: 5000,
-              timerProgressBar: true,
-              didOpen: (toast) => {
-                  toast.addEventListener('mouseenter', Swal.stopTimer)
-                  toast.addEventListener('mouseleave', Swal.resumeTimer)
-               }
-            });
-            e.preventDefault();
-            console.log("Fomulario Enviado....")
-            const {Password, Usuario}=this.state;
-            var datosEnviar={password:Password, usuario:Usuario }
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let usuarioValid = this.state.usuarioValid;
+    let passwordValid = this.state.passwordValid;
+    let confirmacionpasswordValid = this.state.confirmacionpasswordValid;
 
-            axios.post(process.env.REACT_APP_API_ENDPOINT+"ActivarUsuario",datosEnviar)
-                .then((result) => {
-                this.setState({status:true})
-                console.log(result)
-                console.log(result.data);  
+    const MySwal = withReactContent(Swal)
+    const toast = MySwal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 10000,
+      timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    });  
+    switch(fieldName) {
+      case 'Email':
+        usuarioValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.Usuario = usuarioValid ? '' : ' es invalido';
+        break;
+      case 'Password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
+        break;
+      case 'ConfirmacionPassword':
+        confirmacionpasswordValid = value.length >= 6;
+        fieldValidationErrors.ConfirmacionPassword = confirmacionpasswordValid ? '': ' es demasiado corto';
+        break;
+      default:
+        break;
+    }
       
-                  toast.fire({
-                    icon: 'success',
-                    title: ''+result.data.message+'',
-                    confirmButtonText: `Ok`,
-                  })
-                })
-                .catch((error) => {
-                  console.error(error)
-                  console.log(error.response.data.message);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);        
-                    toast.fire({
-                      icon: 'error',
-                      title: ''+error.response.message+'',
-                      confirmButtonText: `Ok`,
-                      })              
-                })
-          }
+    this.setState(
+      {formErrors: fieldValidationErrors,
+        usuarioValid: usuarioValid,
+        passwordValid: passwordValid,
+        confirmacionpasswordValid: confirmacionpasswordValid
+      }, 
+    this.validateForm);
+  }
+  errorClass(error) {
+    return(error.length === 0 ? '' : 'has-error');
+  }
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value},() => { this.validateField(name, value) });
+  }  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid && (this.state.Password = this.state.ConfirmacionPassword)});
+  }
+  handleClickShowPassword = () => {
+    this.setState({showPassword: !this.state.showPassword}) 
+  };
+        
+  handleMouseDownPassword = () => {
+    this.setState({showPassword: this.state.showPassword})
+  };
+         
+  enviarDatos=(e)=>{
+    console.log(this.state.password === this.state.confirmacionPassword)
+  const MySwal = withReactContent(Swal)
+  const toast = MySwal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+     }
+  });
+  e.preventDefault();
+  console.log("Fomulario Enviado....")
+  const {password, Usuario, confirmacionpassword}=this.state;
+  var datosEnviar={password:password, usuario:Usuario, confirmacionpassword:confirmacionpassword}
+  console.log(datosEnviar)
+  axios.post(process.env.REACT_APP_API_ENDPOINT+"ActivarUsuario",datosEnviar)
+    .then((result) => {
+      this.setState({status:true})
+      console.log(result)
+      console.log(result.data);  
+        toast.fire({
+          icon: 'success',
+          title: ''+result.data.message+'',
+          confirmButtonText: `Ok`,
+        })
+    })
+    .catch((error) => {
+      console.error(error)
+      console.log(error.response.data.message);
+      console.log(error.response.status);
+      console.log(error.response.headers);        
+        toast.fire({
+          icon: 'error',
+          title: ''+error.response.message+'',
+          confirmButtonText: `Ok`,
+        })    
+    })
+  }
 
-    render(){
-        return(
-            <section className='contenedorActivacion'>
-                <div className='activarUs'>
-
-                <form className='formActivacion'>
-                    <div className='activacionUsuario'>
-                            <label>Usuario:</label>
-                            <input id='usuario' type='text' placeholder='Introduzca su Correo' className='activacion'></input>
-                        </div>
-                        <div className='activacionContraseña'>
-                            <label>Nueva Contraseña:</label>
-                            <input id='contraseña' type='password' placeholder='Introduzca su Contraseña' className='activacion'></input>
-                        </div>
-                        <div className='activacionConfirmar'>
-                            <label> Confirmar Contraseña:</label>
-                            <input id='confirmar' placeholder='confirmas su Contraseña' type='password' className='activacion confirmar'></input>
-                            <img id="solid_eyeAc" onClick={mostrarC} className='eyeAc' src={eye_solid} alt="Mostrar Contraseña"></img>
-                            <img  id="slash_eyeAc" onClick={mostrarC} className='eyeAc' style={{visibility:'hidden'}} src={eye_slash} alt="Mostrar Contraseña"></img>    
-                        </div>
-                        <button 
-                                onClick={this.enviarDatos}
-                                className='enviarform' 
-                                type='submit'>Confirmar</button>
-                    </form> 
-
-                     
-                </div>
-                
-                
-            </section>
-        )
-    };
+  render(){
+    return(
+      <section className='contenedorActivacion'>
+        <div className='activarUs'>
+          <FormControl>
+            <TextField style={{top:'15%', borderRadius:'2em', background:'#fff'}} id="input-textfield1" label="Correo" type='text' name='Usuario' value={this.state.Usuario} onChange={this.handleUserInput}/>
+            <TextField style={{top:'25%',borderRadius:'2em', background:'#fff'}} id="input-textfield2" label="Contraseña" 
+              type={this.state.showPassword ? 'text' : 'password'}
+              value={this.state.password}
+              onChange={this.handleUserInput}
+              name='password'/>
+            <FormControl style={{height:'auto',width: '100%',top:'35%'}}>
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput id="outlined-adornment-password" type={this.state.showPassword ? 'text' : 'password'} name='confirmacionpassword' value={this.state.confirmacionpassword} onChange={this.handleUserInput}
+                endAdornment={
+                  <IconButton aria-label="toggle password visibility" onClick={this.handleClickShowPassword} onMouseDown={this.handleMouseDownPassword} edge="end">
+                    {this.state.showPassword ? <VisibilityOff/> : <Visibility/>}
+                  </IconButton>
+                }
+              />
+            </FormControl>
+            <Button variant="outlined" disabled={this.state.formValid} onClick={this.enviarDatos}>Confirmar</Button>
+          </FormControl>
+        </div>
+      </section>
+    )
+  };
 };
 export default ActivarUsuario;
 
-
-function mostrarC() {
-    var password = document.getElementById("contraseña");
-    var confirmarPass = document.getElementById("confirmar");
-    const solid = document.getElementById('solid_eyeAc');
-    const slash = document.getElementById('slash_eyeAc');
-console.log(password.type)
-    if (password.type === "password") {
-        password.type = "text";
-        confirmarPass.type = "text";
-        slash.style.visibility = "visible";
-        solid.style.visibility = "hidden"
-    }else {
-        password.type = "password";
-        confirmarPass.type = "password";
-        solid.style.visibility = "visible"
-        slash.style.visibility = "hidden"
-    }
-   };
--- src/componentes/login/login.js
+++ src/componentes/login/login.js
@@ -1,186 +1,138 @@
 import React from 'react';
-import eye_slash from '../../landing/favicon/eye-slash-solid.svg';
-import eye_solid from '../../landing/favicon/eye-solid.svg';
 import './login.css';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content'
-class Login extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-          Email: '',
-          Password: '',
-          formErrors: {Email: '', Password: ''},
-          emailValid: false,
-          passwordValid: false,
-          formValid: false
-        }
-      }
+import { TextField, FormControl, InputLabel, OutlinedInput} from '@mui/material';
+import { Visibility, VisibilityOff} from '@material-ui/icons';
+import { IconButton, Button} from '@mui/material';
 
-      validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let emailValid = this.state.emailValid;
-        let passwordValid = this.state.passwordValid;
+class Login extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      Email: '',
+      Password: '',
+      formErrors: {Email: '', Password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
+    }
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
     
-        switch(fieldName) {
-          case 'Email':
-            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-            fieldValidationErrors.Email = emailValid ? '' : ' es invalido';
-            break;
-          case 'Password':
-            passwordValid = value.length >= 6;
-            fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
-            break;
-          default:
-            break;
-        }
-        this.setState(
-          {
-            formErrors: fieldValidationErrors,
-            emailValid: emailValid,
-            passwordValid: passwordValid
-          }, 
-          this.validateForm
-          );
-      }
-      errorClass(error) {
-        return(error.length === 0 ? '' : 'has-error');
-      }
-      validateForm() {
-        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
-      }
-      
-      handlePassword = (e)=>{
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({[name]: value},() => { this.validateField(name, value) });
-      }
-      handleUserEmail = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({[name]: value},() => { this.validateField(name, value) });
-       }  
-       enviarDatos=(e)=>{ 
-        const MySwal = withReactContent(Swal)
-        const toast = MySwal.mixin({
-          toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 10000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-           }
-        });
-        e.preventDefault();
+    switch(fieldName) {
+      case 'Email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.Email = emailValid ? '' : ' es invalido';
+        break;
+      case 'Password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.Password = passwordValid ? '': ' es demasiado corto';
+        break;
+      default:
+        break;
+    }
+    this.setState(
+      {formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        passwordValid: passwordValid
+      }, 
+      this.validateForm);
+    }
+    errorClass(error) {
+      return(error.length === 0 ? '' : 'has-error');
+    }
+    validateForm() {
+      this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    }
+    handleUserInput = (e) => {
+      const name = e.target.name;
+      const value = e.target.value;
+      this.setState({[name]: value},() => { this.validateField(name, value) });
+    } 
+
+    handleClickShowPassword = () => {
+      this.setState({showPassword: !this.state.showPassword}) 
+    };
+            
+    handleMouseDownPassword = () => {
+      this.setState({showPassword: this.state.showPassword})
+    };
        
-        const {Email,Password}=this.state;
-        var datosEnviar={email:Email,password:Password} 
+    enviarDatos=(e)=>{ 
+      const MySwal = withReactContent(Swal)
+      const toast = MySwal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 10000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      });
+      e.preventDefault();
+      const {Email,Password}=this.state;
+      var datosEnviar={email:Email,password:Password} 
                
-        axios.post(process.env.REACT_APP_API_ENDPOINT+"login",datosEnviar).then(result => {
+      axios.post(process.env.REACT_APP_API_ENDPOINT+"login",datosEnviar).then(result => {
         var nombre=result.data.NombresUsuarios;
         var apellidos=result.data.ApellidosUsuarios;  
         localStorage.setItem('token', result.data.token);
         localStorage.setItem('Login', result.data.Login);    
-        toast.fire({
+      toast.fire({
           icon: 'success',
           title: ''+result.data.message+' '+nombre+' '+apellidos+'',
           confirmButtonText: `Ok`,
-        })    
-        var cliente = result.data.Cliente;
+      })    
+      var cliente = result.data.Cliente;
         if (cliente === 'ATENAS') {
           window.location.href = 'management/panel'
         } else {
           console.log('no')
         }  
-        }).catch(err => {
+      }).catch(err => {
           console.log(err)
-           if (err.response) {
-             console.log(err.response.data.message);
-             console.log(err.response.status);
-             console.log(err.response.headers);        
-           }       
-           toast.fire({
-             icon: 'error',
-             title: ''+err.response.data.message+'',
-             confirmButtonText: `Ok`,
-           })    
+          if(err.response) {
+            console.log(err.response.data.message);
+            console.log(err.response.status);
+            console.log(err.response.headers);        
+          }       
+          toast.fire({
+            icon:'error',
+            title:''+err.response.data.message+'',
+            confirmButtonText: `Ok`,
+          })    
         })
     }
 
 	render() {
-
-        return (
-            <section className='contenedorcard'>
-                <div className="card">
-                    <div className='card-login'>
-                        <form className='form-body' action='login' onSubmit={this.enviarDatos}>
-                            <div className='form'>
-                                <div className='form-group'>
-                                    <input 
-                                     value={this.state.Email} 
-                                     onChange={this.handleUserEmail}
-                                    type='email' 
-                                    name="Email" 
-                                    className='form-control' 
-                                    id='inputEmail' 
-                                    placeholder='Email'></input>
-                                </div>
-
-                                <div className='form-group'>
-                                    <input 
-                                     value={this.state.Password} 
-                                     onChange={this.handlePassword}
-                                     onKeyUp={(e)=>{
-                                        if(e.key === 'Enter'){
-                                          this.enviarDatos(e)
-                                        }
-                                       }
-                                     }
-                                    type='password' 
-                                    name="Password"
-                                    className='form-control' 
-                                    id="inputPassword" 
-                                    placeholder="Password"></input>
-                                    <img id="solid" onClick={mostrarPDW} className='eye-show' src={eye_solid} alt="Mostrar Contraseña"></img>
-                                    <img  id="slash" onClick={mostrarPDW} className='eye-show' style={{visibility:'hidden'}} src={eye_slash} alt="Mostrar Contraseña"></img>
-                                </div>
-                            </div>
-                            <div className='buttons'>
-                                <button
-                                type='button' 
-                                onClick={this.enviarDatos}
-                                disabled={!this.state.formValid}
-                                className='interaction log-in'>
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </section>
-        )
-    };
+    return (
+      <section className='contenedorActivacion'>
+        <div className='activarUs'>
+          <FormControl>
+            <TextField style={{top:'15%',overflow:'visible', borderRadius:'2em', background:'#fff'}} id="input-textfield1" label="Correo" type='text' name='Email' value={this.state.Email} onChange={this.handleUserInput}/>
+            <FormControl style={{height:'auto',overflow:'visible',width: '100%',top:'35%'}}>
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput id="outlined-adornment-password" type={this.state.showPassword ? 'text' : 'password'} name='Password' value={this.state.Password} onChange={this.handleUserInput}
+                endAdornment={
+                  <IconButton aria-label="toggle password visibility" onClick={this.handleClickShowPassword} onMouseDown={this.handleMouseDownPassword} edge="end">
+                    {this.state.showPassword ? <VisibilityOff/> : <Visibility/>}
+                  </IconButton>
+                }
+              />
+            </FormControl>
+            <Button variant="outlined" disabled={!this.state.formValid} onClick={this.enviarDatos}>Confirmar</Button>
+          </FormControl>
+        </div>
+      </section>
+    )
+  };
 }
 export default Login;
-
-    
-   function mostrarPDW() {
-    var password = document.getElementById("inputPassword");
-    const solid = document.getElementById('solid');
-    const slash = document.getElementById('slash');
-
-    if (password.type === "password") {
-        password.type = "text";
-        slash.style.visibility = "visible";
-        solid.style.visibility = "hidden"
-    }else {
-        password.type = "password";
-        solid.style.visibility = "visible"
-        slash.style.visibility = "hidden"
-    }
-   };
-  
-
